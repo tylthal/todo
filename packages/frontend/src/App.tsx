94ff200b0834f355bf9aed5f9452fa60d8009064
Tyler Thalman <tyler.thalman@gmail.com>
2025-06-08 17:21:55 -0600

    Add movable resizable rich notes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UserProvider } from './UserContext';
 import { AccountControls } from './AccountControls';
 import { NoteCanvas } from './NoteCanvas';
 import './App.css';
 
+export interface Note {
+  id: number;
+  content: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  archived: boolean;
+}
+
 const App: React.FC = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const addNote = () => {
+    const id = Date.now();
+    setNotes([
+      ...notes,
+      {
+        id,
+        content: '',
+        x: 40,
+        y: 40,
+        width: 150,
+        height: 120,
+        archived: false
+      }
+    ]);
+  };
+
+  const updateNote = (id: number, data: Partial<Note>) => {
+    setNotes(notes.map(n => (n.id === id ? { ...n, ...data } : n)));
+  };
+
+  const removeNote = (id: number) => {
+    setNotes(notes.filter(n => n.id !== id));
+  };
+
   return (
     <UserProvider>
       <div className="app">
-        <AccountControls />
-        <NoteCanvas />
+        <AccountControls onAddNote={addNote} />
+        <NoteCanvas
+          notes={notes.filter(n => !n.archived)}
+          onDelete={removeNote}
+          onUpdate={updateNote}
+          onArchive={(id) => updateNote(id, { archived: true })}
+        />
       </div>
     </UserProvider>
   );
