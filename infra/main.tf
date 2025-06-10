b233560e43006e559ccf71a7e04ea6e993de75e4
Tyler Thalman <tyler.thalman@gmail.com>
2025-06-09 21:48:30 -0600

    Migrate S3 website configuration

--- infra/main.tf
+++ infra/main.tf
@@ -16,10 +16,17 @@ resource "aws_s3_bucket" "frontend" {
   bucket = var.bucket_name
 
   force_destroy = true
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
   }
 }
 
