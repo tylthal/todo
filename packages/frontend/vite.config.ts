d7d0de862da8f4e80a2361bfae1b834261f157b6
Tyler Thalman <tyler.thalman@gmail.com>
2025-06-10 23:05:34 -0600

    update vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: 'globalThis',
-  },
+    plugins: [react()],
+    define: {
+        global: {},
+    },
+    server: {
+        port: 5173,
+    },
+    resolve: {
+        alias: {
+            './runtimeConfig': './runtimeConfig.browser',
+        },
+    },
 });
