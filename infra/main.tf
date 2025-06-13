84539826d270cde6a0d075beacd1c1218f2f8581
Tyler Thalman <tyler.thalman@gmail.com>
2025-06-13 16:06:19 -0600

    fix api stage method settings

--- infra/main.tf
+++ infra/main.tf
@@ -780,15 +780,20 @@ resource "aws_api_gateway_stage" "main" {
     })
   }
 
-  method_settings {
+
+  depends_on = [aws_api_gateway_account.main]
+}
+
+resource "aws_api_gateway_method_settings" "default" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  stage_name  = aws_api_gateway_stage.main.stage_name
+  method_path = "*/*"
+
+  settings {
     logging_level      = "INFO"
     data_trace_enabled = true
     metrics_enabled    = true
-    http_method        = "*"
-    resource_path      = "/*"
   }
-
-  depends_on = [aws_api_gateway_account.main]
 }
 
 
